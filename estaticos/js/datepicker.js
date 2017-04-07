1edbe486756ea1544a27f9a82f7544d896c4426a
nicoc <norfuen@hotmail.com>
2017-04-06 23:44:20 -0300

    Estructurando carpetas

--- estaticos/js/datepicker.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    $(".datepicker").datepicker({
-        changeDay: true,
-        changeMonth: true,
-        changeYear: true,
-        yearRange: "1990:2030",
-        dateFormat: 'dd/mm/yy'
-    });
-})();
